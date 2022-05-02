6c356f6f87ead4b191ef60704955453775fe513d
HiroyukiShimono <hiroyuki_3_11_23_s@outlook.jp>
2022-05-03 00:13:23 +0900

    third commit
    -added car object
    -added environment and scroll

---
+++ Assets/RepoScene/Scripts/Scroll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    private float scrollSpeed = 5f;
+    private float wallSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject wallObject = gameObject.transform.Find("Wall").gameObject;
+        wallSize = wallObject.transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(scrollSpeed * Time.deltaTime * Vector3.left);
+
+        Vector3 currentPos = transform.position;
+        if (currentPos.x < -wallSize / 2)
+        {
+            transform.position = Vector3.zero;
+        }
+    }
+}
